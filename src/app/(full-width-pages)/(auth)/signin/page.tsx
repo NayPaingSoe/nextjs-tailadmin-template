650b214e75fe2e8ed4618a3dffaed975488de9ab
NayPaingSoe <naypaingsoe042@gmail.com>
2025-07-02 11:27:44 +0630

    fix: signin ui

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -2,8 +2,8 @@ import SignInForm from "@/components/auth/SignInForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "SignIn",
+  description: "This is Signin Page For GiveCredit ",
 };
 
 export default function SignIn() {
